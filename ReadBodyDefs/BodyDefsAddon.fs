e2ef28c2ee059f05b5e8d7e1d5071bc0bf70afd9
Thomas Dufour <tdufour@ankama.com>
2015-03-17 14:17:42 +0100

    [ReadBodyDefs] Addon shell

---
+++ ReadBodyDefs/BodyDefsAddon.fs
@@ -0,0 +1,9 @@
+﻿namespace ReadBodyDefs
+
+open UnityEngine
+
+[<KSPAddon(KSPAddon.Startup.TrackingStation, true)>]
+type BodyDefsAddon() =
+    inherit MonoBehaviour()
+    member self.Start () : unit =
+        MonoBehaviour.print "[ReadBodyDefs] LOADED"
