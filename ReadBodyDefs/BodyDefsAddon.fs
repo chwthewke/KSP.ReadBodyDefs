aeb7e6ea6b619d113689637215187eba7dcd51cc
Thomas Dufour <tdufour@ankama.com>
2015-03-17 18:03:28 +0100

    [ReadBodyRefs] Reading r, T, mu, SoI, color

--- ReadBodyDefs/BodyDefsAddon.fs
+++ ReadBodyDefs/BodyDefsAddon.fs
@@ -7,3 +7,34 @@ type BodyDefsAddon() =
     inherit MonoBehaviour()
     member self.Start () : unit =
         MonoBehaviour.print "[ReadBodyDefs] LOADED"
+
+module Objects =
+    let toOption (obj: 'a): 'a option = if obj = null then None else Some obj
+
+module BodyData =
+    type Body = 
+        { Name: string
+        ; Radius: float
+        ; RotationPeriod: float
+        ; GravitationalParameter: float
+        ; SoI: float option
+        }
+
+    let ReadSoI : CelestialBody -> float option = fun cb ->
+        let soi = cb.sphereOfInfluence
+        in if (soi = 0.0) then None else Some(soi) // Check presence condition
+
+    let ReadColor : CelestialBody -> Color option = fun cb ->
+        cb.orbitDriver 
+            |> Objects.toOption
+            |> Option.bind (fun d -> d.Renderer |> Objects.toOption)
+            |> Option.map (fun r -> r.orbitColor)
+
+    let Read : CelestialBody -> Body = fun cb ->
+        { Name = cb.bodyName
+        ; Radius = cb.Radius
+        ; RotationPeriod = cb.rotationPeriod
+        ; GravitationalParameter = cb.gravParameter
+        ; SoI = ReadSoI cb
+        }
+
